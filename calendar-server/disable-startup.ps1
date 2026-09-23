18f3dd2b9a78658118dedd07afbfd2d58c6e4903
kkoh0508-lgtm <kkoh0508@g.jbedu.kr>
2026-09-23 22:20:08 +0900

    Delete calendar-server directory

--- calendar-server/disable-startup.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$calendarShortcut = Join-Path ([Environment]::GetFolderPath('Startup')) 'Geonoh Calendar.url'
-if (Test-Path -LiteralPath $calendarShortcut) { Remove-Item -LiteralPath $calendarShortcut }
-Write-Output 'Calendar automatic startup is disabled.'
